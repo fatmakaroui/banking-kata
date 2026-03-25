638d347ac75c0540e5d2c36365410e628d2a4328
fatmakaroui <fatmakaroui1@gmail.com>
2026-03-25 20:37:25 +0000

    chore: init project structure with hexagonal architecture

---
+++ backend/src/main/java/com/banking/banking_kata/BankingKataApplication.java
@@ -0,0 +1,13 @@
+package com.banking.banking_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingKataApplication.class, args);
+	}
+
+}

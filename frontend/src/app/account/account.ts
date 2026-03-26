8f05e71345d8db91abc0a4d0c683ec9759eb9d93
fatmakaroui <fatmakaroui1@gmail.com>
2026-03-26 21:30:18 +0000

    feat(green): implement deposit in Account Service

--- frontend/src/app/account/account.ts
+++ frontend/src/app/account/account.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Account {}
+export class Account {
+
+  private readonly apiUrl = 'http://localhost:8080/accounts';
+
+  constructor(private http: HttpClient) {}
+
+  deposit(accountId: string, amount: number): Observable<void> {
+    return this.http.post<void>(
+      `${this.apiUrl}/${accountId}/deposit`,
+      null,
+      { params: { amount: amount.toString() } }
+    );
+  }
+}

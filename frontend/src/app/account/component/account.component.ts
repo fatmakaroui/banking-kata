b7db9bfbde6c4f78050a0e5c62569530c4404d68
fatmakaroui <fatmakaroui1@gmail.com>
2026-03-26 22:54:02 +0000

    feat(green): add deposit method and amount to AccountComponent

--- frontend/src/app/account/component/account.component.ts
+++ frontend/src/app/account/component/account.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../service/account.service';
 
 @Component({
   selector: 'app-account-component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css',
 })
-export class AccountComponent  {}
+export class AccountComponent implements OnInit {
+  statement: string = '';
+  accountId: string = 'account-1';
+  amount: number = 0;
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    this.loadStatement();
+  }
+
+  loadStatement(): void {
+    this.accountService.getStatement(this.accountId).subscribe((data) => (this.statement = data));
+  }
+
+  deposit(): void {
+    this.accountService.deposit(this.accountId, this.amount).subscribe(() => this.loadStatement());
+  }
+}
